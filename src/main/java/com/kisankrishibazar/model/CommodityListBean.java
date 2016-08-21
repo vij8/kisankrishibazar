3a344e0341ac80e202d98f009ef1f7fb97478f13
abhishek.c.tiwari <abhishek.c.tiwari@accenture.com>
2016-08-21 16:01:47 +0530

    Add CommodityListBean

---
+++ src/main/java/com/kisankrishibazar/model/CommodityListBean.java
@@ -0,0 +1,25 @@
+package com.kisankrishibazar.model;
+
+public class CommodityListBean {
+	private String item ;
+	private int quantity;
+	private Double price;
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
